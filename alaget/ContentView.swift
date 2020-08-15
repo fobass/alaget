51e2feac6917bc9618816a3da34180da9841b5fb
fobass <isabekove@gmail.com>
2020-08-16 00:43:58 +0800

    add tabs

--- alaget/ContentView.swift
+++ alaget/ContentView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selection : Int = 0
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        TabView(selection: self.$selection){
+            Departure_View()
+            tabItem {
+                Image(systemName: (selection == 0) ? "magnifyingglass" : "magnifyingglass")
+                    .resizable()
+                    .imageScale(.large)
+                Text("EXPLOER")
+            }
+            .tag(0)
+        }
+        
     }
 }
 
