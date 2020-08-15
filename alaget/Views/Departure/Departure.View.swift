de4e14e0757ab007a107dbf0828b1ecb4e33fd3e
fobass <isabekove@gmail.com>
2020-08-16 00:34:58 +0800

    add views

---
+++ alaget/Views/Departure/Departure.View.swift
@@ -0,0 +1,20 @@
+//
+//  Departure.View.swift
+//  alaget
+//
+//  Created by Ernist Isabekov on 16/08/2020.
+//
+
+import SwiftUI
+
+struct Departure_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Departure_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Departure_View()
+    }
+}
---
+++ alaget/Views/Explorer/Explorer.View.swift
@@ -0,0 +1,20 @@
+//
+//  Explorer.View.swift
+//  alaget
+//
+//  Created by Ernist Isabekov on 16/08/2020.
+//
+
+import SwiftUI
+
+struct Explorer_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Explorer_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Explorer_View()
+    }
+}
