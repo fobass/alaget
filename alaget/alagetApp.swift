db940eab485448fe607c9e03aed01c20f123b04e
fobass <isabekove@gmail.com>
2020-08-16 00:04:03 +0800

    Initial Commit

---
+++ alaget/alagetApp.swift
@@ -0,0 +1,17 @@
+//
+//  alagetApp.swift
+//  alaget
+//
+//  Created by Ernist Isabekov on 16/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct alagetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
